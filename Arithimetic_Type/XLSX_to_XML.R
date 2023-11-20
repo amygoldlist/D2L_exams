0fb9c416c1af3d6c5578360d940c4bf0abb586ef
amygoldlist <amygoldlist@gmail.com>
2023-11-20 09:31:55 -0800

    fixed a regex issue, add comments

--- Arithimetic_Type/XLSX_to_XML.R
+++ Arithimetic_Type/XLSX_to_XML.R
@@ -1,6 +1,6 @@
 #################################################################
 ##
-## Create a zipped xml file to upload to D2L
+## Create a xml folder to upload to D2L - you will need to zip
 ## Input = XLSX (or should it be CSV?)
 ## Amy_note = could we put the title somewhere?  does it matter?
 ##
@@ -15,9 +15,9 @@ library(xml2)
 df <- read.xlsx("data/Base_file.xlsx")
 xml_qq <- read_xml("data/questiondb_base.xml")
 
-############################
-## add the date to the filename,  could expand!
-############################
+#####################################
+## add the date to the filename,  
+#####################################
 
 
 ## 1. find the correct node:
@@ -33,25 +33,23 @@ print("Wrote the title.  Starting the next phase....")
 for (i in (1:nrow(df) )){
   print(paste("Number of questions to get to: ", nrow(df)))
   print(paste("Adding question ", i))
-  #### create a new question?
+  #### create a new question
   
   ## identify the item
   qq_set <- xml_find_all(xml_qq, xpath = ".//item")
   qq_node <- qq_set[i]
   
-  ## this step is unneccessary
-  #new_q <- qq_node
-  
+  ## Metadata about the question:
   xml_attr(qq_node,attr = "title")  <- df$Title[i]
   xml_attr(qq_node,attr = "id") <- df$N[i]
   xml_attr(qq_node,attr = "ident") <- paste0("OBJ_",df$N[i])
   xml_attr(qq_node,attr = "label") <- paste0("QUES_",df$N[i])
   
-  
+  ## insert the question
   question_node <- xml_find_first(qq_node, xpath = ".// mattext")
   xml_text(question_node) <- df$Question[i]
   
-  
+  ## Insert the answer / formula - exists in multiple places.
   ans_set <- xml_find_all(qq_node,xpath = ".// formula")
   for (j in (1:length(ans_set))){
     ans_node <- ans_set[j]
@@ -59,20 +57,16 @@ for (i in (1:nrow(df) )){
   }
   
 
+  ## More labelling, in the hint adn feedback:
   
   qnode2 <- xml_find_first(qq_node, xpath = ".//response_num")
   xml_attr(qnode2, attr = "ident") <- paste0("QUES_",df$N[i],"_NUM")
   
   qnode2 <- xml_find_first(qq_node, xpath = ".//response_label")
-  ##inspect
-  #xml_attrs(qnode2)
   xml_attr(qnode2, attr = "ident") <- paste0("QUES_",df$N[i],"_A1")
   
-  
-  ### This is worthy of a function!!
+
   qnode2 <- xml_find_first(qq_node, xpath = ".//response_str")
-  ##inspect
-  #xml_attrs(qnode2)
   xml_attr(qnode2, attr = "ident") <- paste0("QUES_",df$N[i],"_STR")
   
   
@@ -81,27 +75,26 @@ for (i in (1:nrow(df) )){
   #xml_attrs(qnode2)
   
   xml_text(qnode2) <- as.character(df$Precision[i])
+  ## I don't liek the precision enforced, but one could (Why?) add this in:
   #xml_attr(qnode2, attr = "precision_enforced") <- "no"
   
   qnode2 <- xml_find_all(qq_node, xpath = ".//tolerance")
-  xml_attrs(qnode2)
-  
+  #xml_attrs(qnode2) #uncomment to inspect.
   xml_text(qnode2) <- as.character(df$Tolerance[i])
   
-
   
-  ## feedback?
+  ## feedback
   qnode2 <- xml_find_all(qq_node, xpath = ".//itemfeedback")
   qnode3 <- xml_find_all(qnode2, xpath = ".//mattext")
   xml_text(qnode3) <-  df$feedback[i]
   xml_attr(qnode2, attr = "ident") <- paste0("QUES_", df$N[i])
  
-  ## hint?
+  ## hint
   qnode2 <- xml_find_all(qq_node, xpath = ".//hintmaterial") 
   qnode3 <- xml_find_all(qnode2, xpath = ".//mattext")
   xml_text(qnode3) <- df$Hint[i]
   
-  ## maybe create a new node?
+  ## Create a new question node, of this wasn't the last question.
   if (i< nrow(df)){
     print("creating a new question")
     xml_add_sibling(qq_node, .value =qq_node, where = "after", .copy = TRUE)
@@ -110,19 +103,16 @@ for (i in (1:nrow(df) )){
 
 
 ### Print to file
-  
 
-##write_xml(xml_qq, "hi.xml")  #Test!
-
-
-## The whole kit and caboodle
 ## create a new folder:
-folder_name <- paste0("output\arithmetic_section_", Sys.Date())
+folder_name <- paste0("output\\arithmetic_section_", Sys.Date())
 dir.create(folder_name)
-write_xml(xml_qq, paste0(folder_name,"\\questiondb.xml"))
-## move the other file?
 
-manifest <- read_xml("data\imsmanifest.xml")
+## Write file number one
+write_xml(xml_qq, paste0(folder_name,"\\questiondb.xml"))
+
+## copy and write file number 2
+manifest <- read_xml("data\\imsmanifest.xml")
 write_xml(manifest, paste0(folder_name,"\\imsmanifest.xml"))
 
 
